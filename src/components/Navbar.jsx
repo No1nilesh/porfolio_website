deb7b60c477b0a6933452fd5f8d638f14aa2ca94
Nilesh Gautam <gautamnilesh084@gmail.com>
2024-08-03 16:54:04 +0530

    added to externals

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,39 @@
-import { useState, useEffect, useRef } from 'react';
-import useGSAP from './Utility/useGSAPP';
-import useLenis from './Utility/useLenis';
-import useDebounce from './Utility/useDebounce';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse} from '@fortawesome/free-solid-svg-icons/faHouse'
-import { faUser } from '@fortawesome/free-solid-svg-icons/faUser'
-import { faToolbox,  } from '@fortawesome/free-solid-svg-icons/faToolbox'
-import { faEnvelope, } from '@fortawesome/free-solid-svg-icons/faEnvelope'
-import { faBook } from '@fortawesome/free-solid-svg-icons/fabook'
-import { useCallback } from 'react';
-
-
-
+import { useState, useEffect, useRef, useCallback } from "react";
+import useGSAP from "./Utility/useGSAPP";
+import useLenis from "./Utility/useLenis";
+import useDebounce from "./Utility/useDebounce";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons/faHouse";
+import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
+import { faToolbox } from "@fortawesome/free-solid-svg-icons/faToolbox";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons/faEnvelope";
+import { faBook } from "@fortawesome/free-solid-svg-icons/fabook";
 
 function Navbar() {
-  
   const lenis = useLenis();
   const debounce = useDebounce();
   const [firstVisit, setFirstVisit] = useState(false);
   const [buttonChanged, setbuttonChanged] = useState(false);
-  const [activeSection, setActiveSection] = useState('home');
+  const [activeSection, setActiveSection] = useState("home");
   const ref = useRef();
 
-
-
-//checking user to visit app for first time
+  //checking user to visit app for first time
   useEffect(() => {
     // Check if the user has visited the page before
-    const hasVisitedBefore = localStorage.getItem('visitedBefore');
+    const hasVisitedBefore = localStorage.getItem("visitedBefore");
     if (!hasVisitedBefore) {
       // If not visited before, set the flag and mark it as visited
       setFirstVisit(true);
-      localStorage.setItem('visitedBefore', true);
+      localStorage.setItem("visitedBefore", true);
     }
   }, []);
-
-
 
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
     const elId = "#" + element.id;
     element ? lenis.scrollTo(elId) : null;
-  }
+  };
 
-
-  
   const toggleActive = useCallback(() => {
     let currentActiveButton = null;
     const sections = document.querySelectorAll(".sections");
@@ -56,34 +45,102 @@ function Navbar() {
       }
     });
     setActiveSection(currentActiveButton);
-  }, []); 
-  
-
+  }, []);
 
   const debouncedToggleActive = debounce(toggleActive, 10);
-  useGSAP({gsapAnimation : (gsap)=>{
-    const tl = gsap.timeline({defaults: {ease: "power4.inOut", duration: 2}});
-    tl.to(ref.current, {'clipPath': 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)', opacity: 1, y:0, duration:2.2 })
-  }})
-
+  useGSAP({
+    gsapAnimation: (gsap) => {
+      const tl = gsap.timeline({
+        defaults: { ease: "power4.inOut", duration: 2 },
+      });
+      tl.to(ref.current, {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        opacity: 1,
+        y: 0,
+        duration: 2.2,
+      });
+    },
+  });
 
   useEffect(() => {
-    window.addEventListener('scroll', debouncedToggleActive,{ passive: true } );
-    if(activeSection !== 'home') setbuttonChanged(true);
+    window.addEventListener("scroll", debouncedToggleActive, { passive: true });
+    if (activeSection !== "home") setbuttonChanged(true);
     return () => {
-      window.removeEventListener('scroll', debouncedToggleActive);
+      window.removeEventListener("scroll", debouncedToggleActive);
     };
   }, [debouncedToggleActive, activeSection]);
 
-
   return (
-    <nav ref={ref} className='fixed bottom-2  z-50   xl:w-1/3 clip translate-y-20 opacity-0'>
-      <ul className='flex gap-6 justify-center items-center  border-gray-600 py-2 px-4 rounded-full  glass xl:gap-12'>
-        <li onClick={() => scrollToSection('home')} className={`cursor-pointer transition-transform ${activeSection === 'home' ? "active" : ''}`}><FontAwesomeIcon icon={faHouse} key={"home"}  style={{ color: "#b9c7df", width: "24px" }} /></li>
-        <li onClick={() => scrollToSection('about')} className={`cursor-pointer transition-transform ${activeSection === 'about' ? "active" : ''}  ${activeSection === 'home' && firstVisit && !buttonChanged && 'animate-bounce'}`}><FontAwesomeIcon icon={faUser} key={"about"}  style={{ color: "#b9c7df", width: "24px" }} /></li>
-        <li onClick={() => scrollToSection('experience')} className={`cursor-pointer transition-transform ${activeSection === 'experience' ? "active" : ''}`}><FontAwesomeIcon icon={faBook} key={"experience"}  style={{ color: "#b9c7df", width: "24px" }} /></li>
-        <li onClick={() => scrollToSection('project')} className={`cursor-pointer transition-transform ${activeSection === 'project' ? "active" : ''}`}><FontAwesomeIcon icon={faToolbox} key={"project"}  style={{ color: "#b9c7df", width: "24px" }} /></li>
-        <li onClick={() => scrollToSection('contact')} className={`cursor-pointer transition-transform ${activeSection === 'contact' ? "active" : ''}`}><FontAwesomeIcon icon={faEnvelope} key={"contact"}  style={{ color: "#b9c7df", width: "24px" }} /></li>
+    <nav
+      ref={ref}
+      className="fixed bottom-2  z-50   xl:w-1/3 clip translate-y-20 opacity-0"
+    >
+      <ul className="flex gap-6 justify-center items-center  border-gray-600 py-2 px-4 rounded-full  glass xl:gap-12">
+        <li
+          onClick={() => scrollToSection("home")}
+          className={`cursor-pointer transition-transform ${
+            activeSection === "home" ? "active" : ""
+          }`}
+        >
+          <FontAwesomeIcon
+            icon={faHouse}
+            key={"home"}
+            style={{ color: "#b9c7df", width: "24px" }}
+          />
+        </li>
+        <li
+          onClick={() => scrollToSection("about")}
+          className={`cursor-pointer transition-transform ${
+            activeSection === "about" ? "active" : ""
+          }  ${
+            activeSection === "home" &&
+            firstVisit &&
+            !buttonChanged &&
+            "animate-bounce"
+          }`}
+        >
+          <FontAwesomeIcon
+            icon={faUser}
+            key={"about"}
+            style={{ color: "#b9c7df", width: "24px" }}
+          />
+        </li>
+        <li
+          onClick={() => scrollToSection("experience")}
+          className={`cursor-pointer transition-transform ${
+            activeSection === "experience" ? "active" : ""
+          }`}
+        >
+          <FontAwesomeIcon
+            icon={faBook}
+            key={"experience"}
+            style={{ color: "#b9c7df", width: "24px" }}
+          />
+        </li>
+        <li
+          onClick={() => scrollToSection("project")}
+          className={`cursor-pointer transition-transform ${
+            activeSection === "project" ? "active" : ""
+          }`}
+        >
+          <FontAwesomeIcon
+            icon={faToolbox}
+            key={"project"}
+            style={{ color: "#b9c7df", width: "24px" }}
+          />
+        </li>
+        <li
+          onClick={() => scrollToSection("contact")}
+          className={`cursor-pointer transition-transform ${
+            activeSection === "contact" ? "active" : ""
+          }`}
+        >
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            key={"contact"}
+            style={{ color: "#b9c7df", width: "24px" }}
+          />
+        </li>
       </ul>
     </nav>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,27 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import purgeIcons from 'vite-plugin-purge-icons';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import purgeIcons from "vite-plugin-purge-icons";
 
 export default defineConfig({
-  plugins: [react(),
-    purgeIcons()
-  ],
+  plugins: [react(), purgeIcons()],
   build: {
     chunkSizeWarningLimit: 1500, // Set it to a higher value like 1000 (1 MB)
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
+          if (id.includes("node_modules")) {
+            return "vendor";
           }
         },
       },
+      external: ["@fortawesome/free-solid-svg-icons/fabook"],
     },
-    outDir: 'build', // Set the output directory to 'build'
+    outDir: "build", // Set the output directory to 'build'
   },
-  assetsInclude: ['src/assets/**/*'], // Additional config for asset handling
+  assetsInclude: ["src/assets/**/*"], // Additional config for asset handling
 
   optimizeDeps: {
-    exclude: ['@fortawesome/fontawesome-svg-core'],
+    exclude: ["@fortawesome/fontawesome-svg-core"],
   },
 });
